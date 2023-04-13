697f9f4d40e659a22a923daff8acdbb87dcdea36
gabrielamarqs <gabriela2013.ms@gmail.com>
2023-04-13 15:39:58 -0300

    CRUD finalizado

--- DaoVeiculo.java
+++ DaoVeiculo.java
@@ -1,10 +1,15 @@
 import java.sql.Statement;
+import java.util.ArrayList;
+
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DaoVeiculo {
     private Connection conn;
     private Statement st;
+
+    // vai ficar todas minhas querys 
 
     private void conectar() {
         try {
@@ -43,4 +48,93 @@ public class DaoVeiculo {
         }
         return resultado;
     }
+
+    public ArrayList<Veiculo> buscarTodos(){
+        ArrayList<Veiculo> resultado = new ArrayList<Veiculo>();
+        // criei meu arraylist vazio
+        try {
+            this.conectar();
+            // inserir, editar ou excluir
+            // executeUpdate 
+            ResultSet rs = st.executeQuery("SELECT * FROM tb_veiculos ORDER BY modelo;");
+            // resultset sempre vai retornar uma tabela com dados do que você pediu
+            while (rs.next()) {
+                // enquanto tiver linha no rs vai continuar rodando
+                Veiculo v = new Veiculo();
+                v.setCodigo(rs.getInt("codigo"));
+                v.setMarca(rs.getString("marca"));
+                v.setModelo(rs.getString("modelo"));
+                v.setChassi(rs.getString("chassi"));
+                v.setAno(rs.getInt("ano"));
+                resultado.add(v);
+                // adicionei minha arraylist no meu objeto veiculo
+            }
+            
+        } catch (Exception e1) {
+            System.out.println("Error: " + e1.getMessage());
+        } finally {
+            this.desconectar();
+        }
+        return resultado;
+    }
+
+    public int excluir(int cod) {
+        // vai receber retorno 0 ou 1
+        // se excluiu ou não excluiu
+        int qtd = 0;
+        try {
+            this.conectar();
+            String comando = "DELETE FROM tb_veiculos WHERE codigo = " + cod + ";";
+            st.executeUpdate(comando);
+            qtd = st.getUpdateCount();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            this.desconectar();
+        }
+        return qtd;
+    }
+
+    public Veiculo consultar(int cod){
+        // public -Veiculo- consultar
+        // Veiculo é o que eu quero retornar
+        Veiculo v = null;
+        // inicializar com null para não dizer que o código não foi inicializado
+        // caso fosse  null ia retornar qualquer coisa
+        try {
+            this.conectar();
+            ResultSet rs = st.executeQuery("SELECT * FROM tb_veiculos WHERE codigo = " + cod + ";");
+            if(rs.next()) {
+                v = new Veiculo();
+                v.setCodigo(rs.getInt("codigo"));
+                v.setMarca(rs.getString("marca"));
+                v.setModelo(rs.getString("modelo"));
+                v.setChassi(rs.getString("chassi"));
+                v.setAno(rs.getInt("ano"));
+            }
+        } catch (Exception e1) {
+            System.out.println("Error: " + e1.getMessage());
+        } finally {
+            this.desconectar();
+        }
+        return v;
+    }
+
+    public int atualizar(Veiculo v) {
+        int qtd = 0;
+        try {
+            this.conectar();
+            String comando = "UPDATE tb_veiculos SET marca ='" + v.getMarca() + " ',  modelo ='" + v.getModelo() + "', chassi = '" + v.getChassi() + "', ano = " + v.getAno() + " WHERE codigo = " + v.getCodigo() + ";";
+            System.out.println(comando);
+            st.executeUpdate(comando);
+            qtd = st.getUpdateCount();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            this.desconectar();
+        }
+        return qtd;
+    }
+
+
 }
--- GerenciadorVeiculo.java
+++ GerenciadorVeiculo.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GerenciadorVeiculo {
@@ -31,19 +32,23 @@ public class GerenciadorVeiculo {
             }
             switch (opcao) {
                 case 1:
-                    this.Cadastrar();
+                    this.cadastrar();
                     break;
                 
                 case 2:
+                    this.consultar();
                     break;
 
                 case 3:
+                    this.alterar();
                     break;
                     
                 case 4:
+                    this.excluir();;
                     break;
                     
                 case 5:
+                    this.listar();
                     break;
                     
                 case 0:
@@ -57,7 +62,7 @@ public class GerenciadorVeiculo {
 
     }
 
-    public void Cadastrar() {
+    public void cadastrar() {
         Veiculo v = new Veiculo();
         System.out.println("----------------------");
         System.out.println("[Cadastro de Veiculos]");
@@ -78,5 +83,85 @@ public class GerenciadorVeiculo {
         }
 
     }
+
+    public void listar() {
+        ArrayList<Veiculo> veiculos = daoVeiculo.buscarTodos();
+        System.out.println("==========================");
+        System.out.println("Veiculos cadastrados");
+        for(Veiculo v : veiculos) {
+            System.out.println("Código: " + v.getCodigo() + ", Marca: " + v.getMarca() + ", Modelo: " + v.getModelo() + ", Chassi: " + v.getChassi() + ", Ano: " + v.getAno() + ".");
+        }
+    }
+
+    public void excluir() {
+        System.out.println("==========================");
+        System.out.println("Exclusão de veiculos");
+        System.out.println("Código: ");
+        int codigo = Integer.parseInt(scanner.nextLine());
+        int qtd = daoVeiculo.excluir(codigo);
+        if(qtd > 0) {
+            System.out.println("Excluido com sucesso.");
+        } else {
+            System.out.println("Não encontrado.");
+        }
+    }
+
+    public void consultar() {
+        System.out.println("==========================");
+        System.out.println("Consulta de veiculos");
+        System.out.println("Código: ");
+        int codigo = Integer.parseInt(scanner.nextLine());
+        Veiculo v = daoVeiculo.consultar(codigo);
+        if(v != null) {
+            System.out.println("Código: " + v.getCodigo() + ", Marca: " + v.getMarca() + ", Modelo: " + v.getModelo() + ", Chassi: " + v.getChassi() + ", Ano: " + v.getAno() + ".");        
+        } else {
+            System.out.println("Não encontrado.");
+        }
+    }
+
+    public void alterar() {
+        System.out.println("----------------------");
+        System.out.println("[Alteração de Veiculos]");
+        System.out.println("Código: ");
+        int codigo = Integer.parseInt(scanner.nextLine());
+        Veiculo v = daoVeiculo.consultar(codigo);
+        if (v != null) {
+            System.out.println("Dados do veículo");
+            System.out.println("[Código: " + v.getCodigo() + "]");
+            System.out.println("[Modelo: " + v.getModelo() + "]");
+            String modelo = scanner.nextLine();
+            if (!modelo.isEmpty()) {
+                v.setModelo(modelo);
+            }
+            System.out.println("[Marca: " + v.getMarca() + "]");
+            String marca = scanner.nextLine();
+            if(!marca.isEmpty()) {
+                v.setMarca(marca);
+            }
+
+            System.out.println("[Chassi: " + v.getChassi() + "]");
+            String chassi = scanner.nextLine();
+            if(!chassi.isEmpty()) {
+                v.setChassi(chassi);
+            }
+
+            System.out.println("[Ano: " + v.getAno() + "]");
+            String ano = scanner.nextLine();
+            if(!ano.isEmpty()){
+                v.setAno(Integer.parseInt(ano));
+            }
+            
+            // inserir no banco
+            int qtdAlterado = daoVeiculo.atualizar(v);
+            if(qtdAlterado > 0) {
+                System.out.println("Atualizado com sucesso");
+            } else {
+                System.out.println("Não encontrado.");
+            }
+
+        }
+
+    }
+
 
 }
